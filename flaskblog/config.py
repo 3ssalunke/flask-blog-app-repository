38edf207778779e0c65c5d08e2197eeedc85dc81
surajs <3ssalunke>
2020-11-02 18:28:47 +0530

    Created flask Blog App

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = '77df9a7350eddcb413966a8d9a273285'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '3salunk5@gmail.com'
+    MAIL_PASSWORD = '$Abkonsapass0799'
